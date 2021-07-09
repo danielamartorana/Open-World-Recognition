dd4a3b87c95d369cca2667260292aaa0013d9b4a
Alessia Scandaliato <64848396+Alessia-Sc@users.noreply.github.com>
2021-07-09 17:40:02 +0200

    Add files via upload

---
+++ ResNet_cbam.py
@@ -0,0 +1,155 @@
+import torch.nn as nn
+import math
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, cbam=False, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+        
+        self.ca = ChannelAttention(planes)
+        self.sa = SpatialAttention()
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        #if self.cbam:
+            #out = self.ca(out) * out
+            #out = self.sa(out) * out
+        
+        
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes):
+        self.old_model = None
+        self.num_classes = num_classes
+        self.inplanes = 16 #64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+        self.avgpool = nn.AvgPool2d(8, stride=1)
+        self.fc = nn.Linear(64 * block.expansion, self.num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x, features=False):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        
+        if features:
+            x = x / x.norm()  #estrazione features, vedere se normalizzare o no, non c'è il FC perché in ICARL faccio il classificatore a mano
+        else:
+            x = self.fc(x)   #classificazione
+
+        return x
+
+
+def resnet32(pretrained=False, **kwargs):
+    n = 5
+    model = ResNet(BasicBlock, [n, n, n], **kwargs)
+    return model
+
+def resnetCBAM (pretrained = False, **kwargs):
+    n=5
+    model = ResNet(BasicBlock(cbam=True), [n,n,n], **kwargs)
+    return model
+
+
+class ChannelAttention(nn.Module):
+    def _init_(self, in_planes, ratio=16):
+        super(ChannelAttention, self)._init_()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.sequential = nn.Sequential(
+                   
+        Flatten(),
+        nn.Linear(in_planes, in_planes // 16, bias=False),
+        nn.ReLU(),
+        nn.Linear(in_planes // 16, in_planes,  bias=False),
+        )
+    def forward(self, x):
+        avg_out = self.sequential(self.avg_pool(x))
+        max_out = self.sequential(self.max_pool(x))
+        out = avg_out + max_out
+        return F.sigmoid( out ).unsqueeze(2).unsqueeze(3).expand_as(x)
+
+class SpatialAttention(nn.Module):
+    def _init_(self, kernel_size=7):
+        super(SpatialAttention, self)._init_()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
